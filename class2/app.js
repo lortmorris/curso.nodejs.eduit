5bc8be0f2563a6d7a18d52ec660b24570b3716d9
cesar <cesarcasas@bsdsolutions.com.ar>
2016-06-23 20:58:35 -0300

    separamos todo prototypes

--- class2/app.js
+++ class2/app.js
@@ -1,11 +1,12 @@
 console.log("Servidor web 1.0, class2");
 
 const http = require('http');
-const fs = require("fs");
 const myserver = require("./lib/server");
 
 
-const server = http.createServer(myserver);
+const server = http.createServer((req, res)=>{
+	new myserver(req, res);
+});
 
 server.on('clientError', (err, socket) => {
   socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
--- class2/lib/server.js
+++ class2/lib/server.js
@@ -6,18 +6,39 @@ function server(req, res){
 
 	this.req = req;
 	this.res = res;
- console.log(req.url);
-  let file="";
-  var content="";
-  if(req.url=="/") file="/index.html";
-  try{
-  	content = fs.readFileSync(publicDirectory+file);
-  }catch(e){
-  	content = fs.readFileSync(publicDirectory+"/404.html");
-  }
+ 	console.log(req.url);
   
-  res.end(content);
+  if(!this.staticFile()){
+  	if(!this.dinamicFile()){
+  		this.notFound();
+  	}
+  }
 
 }
 
+
+server.prototype.staticFile = function(){
+	console.log("is static?");
+	var _this = this;
+	let file="";
+  var content="";
+  if(_this.req.url=="/") file="/index.html";
+  try{
+  	content = fs.readFileSync(publicDirectory+file);
+  	return _this.res.end(content);
+  }catch(e){
+  	return null;
+  }
+ 
+}
+
+server.prototype.dinamicFile = function(){
+	console.log("is dinamic?");
+	return null;
+}
+
+server.prototype.notFound = function(){
+	console.log("404");
+	this.res.end(fs.readFileSync(publicDirectory+"/404.html"));
+}
 module.exports = server;
