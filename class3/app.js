80815b591ea1c64b43ea0879fcfefad66dd71885
cesar casas <cesarcasas@bsdsolutions.com.ar>
2016-06-30 20:49:32 -0300

    first form using mongodb

--- class3/app.js
+++ class3/app.js
@@ -2,23 +2,19 @@ console.log("class3");
 
 const express = require("express");
 const app = express();
-
 const mongojs = require('mongojs');
+const bodyParser = require("body-parser");
 const db = mongojs("test", ["films", "users", "test"]);
 
-
-
+app.use(bodyParser.urlencoded({ extended: false }))
 
 function mymws (req, res, next){
 	res.set("auth" , "cesar");	
 	next();
 }
 
-
 app.use(mymws);
-
 app.use(express.static("./public"));
-
 
 app.get("/person", (req, res, next)=>{
 	db.test.find({},{}, (err, docs)=>{
@@ -37,6 +33,17 @@ app.get("/", (req, res)=>{
 });
 
 
+app.post("/save", (req, res)=>{
+	let toSave = {
+		name: req.body.name || "noname",
+		age: req.body.age || 0,
+		dni: req.body.dni || "nodni",
+	};
+
+	db.test.save(toSave, (err, doc)=>{
+		res.redirect("/");
+	});
+})
 
 
 
