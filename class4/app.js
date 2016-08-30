63a09c0cc5381bb8a7de986dd0aa9033fec578c5
Cesar Casas <cesarcasas@bsdsolutions.com.ar>
2016-08-30 18:07:46 -0300

    class 4, socket.io

---
+++ class4/app.js
@@ -0,0 +1,69 @@
+const http = require('http');
+const fs = require('fs');
+const mongojs = require("mongojs");
+const db = mongojs("mongodb://10.10.20.129/chat", ["messages"]);
+const express = require("express");
+const app = express();
+const exphbs  = require('express-handlebars');
+
+app.use(express.static("./public"));
+
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+server.listen(8000);
+
+var connected = {};
+
+
+app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout:'default'}));
+app.set('view engine', '.hbs');
+
+
+app.get("/", (req, res)=>{
+    db.messages.find({own:'cesar'}, {}, (err, docs)=>{
+        res.render("index", {messages: docs});
+    });
+
+});
+
+const sendMsg = (msg, socket)=> {
+    db.messages.insert({
+            msg: msg,
+            added: new Date(),
+            socketid: socket.id,
+            own: "cesar"
+        }
+        , (err, doc)=> {
+            console.log(err, doc);
+            io.emit("msg", doc);
+
+        });
+
+
+}
+
+io.on('connection', function (socket) {
+    connected[socket.id] = {
+        socket: socket,
+        added: new Date()
+    };
+
+    io.emit("new user", {socketid: socket.id});
+
+    socket.on('msg', data=> sendMsg(data, socket));
+
+    socket.on('getLost', (id)=>{
+
+        db.messages.find({ $lt: {_id: db.ObjectId(id) }}).sort({_id:-1}, (err, docs)=>{
+            console.log(err, docs);
+            socket.emit("recover", docs);
+        })
+    });
+
+
+    socket.on('disconnect', ()=> {
+        delete connected[socket.id];
+    });
+
+
+});
---
+++ class4/public/app.js
@@ -0,0 +1,25 @@
+console.log("app init...");
+
+var lastId = null;
+var socket = io({
+    'reconnect': function () {
+        console.log("reconnected");
+        socket.emit("getLost", lastId);
+    }
+});
+
+function addMsg(msg) {
+    $("#messagesPanel").append("<li><span>" + msg.added + "</span>: " + msg.msg + "</li>");
+}
+
+socket.on('msg', function (data) {
+    let msg = data;
+    lastId = data._id;
+    addMsg(msg);
+});
+
+
+function send(msg) {
+    socket.emit("msg", msg);
+}
+
