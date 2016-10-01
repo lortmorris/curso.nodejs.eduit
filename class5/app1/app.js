05a23f1f817f7bc815c19bef858813333dec200c
cesar casas <cesarcasas@bsdsolutions.com.ar>
2016-09-30 21:27:09 -0300

    little changes

--- class5/app1/app.js
+++ class5/app1/app.js
@@ -6,15 +6,35 @@ const io = require('socket.io')(server);
 
 app.use(express.static("./public"));
 
+const makeColor = (str, color)=> "<span style='color:"+color+"'>"+str+"</span>";
+
+const activeUsers = {};
+
+const emit=(id, msg, skip)=>{
+	skip = skip || null;
+
+	for(k in activeUsers){
+		if(skip!=k)	{
+			if(msg.indexOf(k)>-1) msg = makeColor(msg,"red");
+			msg = msg.replace(/\/\/alegria/ig, "<img class='icon' src='http://i60.photobucket.com/albums/h34/ramarcher/Ballons%203te/Unbenannt-1-1.jpg' width='16' > ");
+			activeUsers[k].emit("message", id+": "+msg);
+		}
+	}
+};
+
 io.on('connection', (socket)=>{
   console.log('a user connected: ', socket.id);
+	activeUsers[socket.id] = socket;
 
+	socket.emit("message", Object.keys(activeUsers).join("<br />"));
   socket.on('disconnect', ()=>{
+  	delete activeUsers[socket.id];
     console.log('user disconnected: ', socket.id);
   });
 	
   socket.on('message', (msg)=>{
-  	io.emit('message', socket.id+": "+msg);
+  	emit(socket.id, msg,  socket.id);
+  	socket.emit("message",makeColor(socket.id+": "+msg, "blue"));
   });
 	
 });
