c1eb3d43b846eb0b2eb29b1a8483c0d389195fd0
Cesar Casas <cesarcasas@bsdsolutions.com.ar>
2016-12-21 15:23:45 -0300

    all data from db

--- class3/abmtojson/app.js
+++ class3/abmtojson/app.js
@@ -11,59 +11,49 @@ const db = mongojs('mongodb://localhost/mexico', ['alumnos']);
 app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout: 'default'}) );
 app.set('view engine', '.hbs');
 
-
 let counter = 0;
-let documentRoot = 'public';
-let alumnos = [];
-let dbfile = 'data.json';
+
 
 app.use( express.static('./public') );
 app.use(bodyParser.urlencoded({ extended: false }));
 
-fs.readFile('./'+dbfile, (err, data)=>{
-  if(!err && data.toString()!='') alumnos = JSON.parse(data.toString());
-});
-
-
-let showHome = (DB, req, res)=>{
-  let table= '';
-  res.render('home', {alumnos: DB});
-};
-
-
 app.get('/counter', (req, res)=> res.end('counter: '+counter++));
 app.get('/datetime', (req, res)=> res.end(new Date().toString));
-app.get('/', (req, res)=> showHome(alumnos, req, res));
+app.get('/', (req, res)=> {
+  db.alumnos.find({}, {}, (err, docs)=>{
+    res.render('home', {alumnos: docs});
+  });
+
+});
+
 app.get('/alumno/:curp', (req, res)=>{
-  let alumno = alumnos.filter(a=> a.curp == req.params.curp);
-  if(alumno.length==0) return res.redirect('/');
+  db.alumnos.findOne( {curp: req.params.curp}, {}, (err, doc)=>{
+    res.render('profile', {alumno:  doc});
+  });
+});
 
-  res.render('profile', {alumno:  alumno[0]});
-
-})
 app.get('/add', (req, res)=>{
   res.render('add');
 });
 
 app.post('/save', (req, res)=>{
   req.query.added = new Date();
-
   db.alumnos.insert(req.body, (err, doc)=>{
-    if(err) console.log(err);
-    else console.log(doc);
+    res.redirect('/');
   });
-  showHome(alumnos, req, res);
 });
 
 app.get('/filter', (req, res)=>{
-  showHome(alumnos.filter(a=> a.prom == req.query.prom), req, res)
+  db.alumnos.find({prom: req.query.prom}, {}, (err, docs)=>{
+    res.render('home', {alumnos: docs});
+  });
 });
 
 app.get('/delete', (req, res)=>{
-  alumnos = alumnos.filter(a=> a.curp != req.query.curp);
-  saveDB(alumnos);
-  showHome(alumnos, req, res);
-})
+  db.alumnos.remove({_id: db.ObjectId(req.query._id)}, (err, doc)=>{
+    res.redirect('/');
+  });
+});
 
 console.log('listen');
 server.listen(5000);
