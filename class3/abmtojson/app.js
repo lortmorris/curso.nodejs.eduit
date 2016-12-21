9f373029edc842f6fae733af10f97b5d38dee791
Cesar Casas <cesarcasas@bsdsolutions.com.ar>
2016-12-21 13:23:16 -0300

    abmtojson with express and handlebars

---
+++ class3/abmtojson/app.js
@@ -0,0 +1,63 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs  = require('express-handlebars');
+const app = express();
+const server = http.createServer(app);
+const fs = require('fs');
+
+app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout: 'default'}) );
+app.set('view engine', '.hbs');
+
+
+let counter = 0;
+let documentRoot = 'public';
+let alumnos = [];
+let dbfile = 'data.json';
+
+app.use( express.static('./public') );
+app.use(bodyParser.urlencoded({ extended: false }));
+
+fs.readFile('./'+dbfile, (err, data)=>{
+  if(!err && data.toString()!='') alumnos = JSON.parse(data.toString());
+});
+
+
+let saveDB = (data)=>{
+  fs.writeFile('./'+dbfile, JSON.stringify(data), (err)=>{
+      //do something
+  });
+}
+
+let showHome = (DB, req, res)=>{
+  let table= '';
+  res.render('home', {alumnos: DB});
+};
+
+
+app.get('/counter', (req, res)=> res.end('counter: '+counter++));
+app.get('/datetime', (req, res)=> res.end(new Date().toString));
+app.get('/', (req, res)=> showHome(alumnos, req, res));
+app.get('/add', (req, res)=>{
+  res.render('add');
+});
+
+app.post('/save', (req, res)=>{
+  req.query.added = new Date();
+  alumnos.push(req.body);
+  saveDB(alumnos);
+  showHome(alumnos, req, res);
+});
+
+app.get('/filter', (req, res)=>{
+  showHome(alumnos.filter(a=> a.prom == req.query.prom), req, res)
+});
+
+app.get('/delete', (req, res)=>{
+  alumnos = alumnos.filter(a=> a.curp != req.query.curp);
+  saveDB(alumnos);
+  showHome(alumnos, req, res);
+})
+
+console.log('listen');
+server.listen(5000);
