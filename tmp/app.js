12331cb4e175c3b5a8d1040bffcd0bbfdab9edc6
Cesar Casas <cesarcasas@bsdsolutions.com.ar>
2017-06-27 19:07:30 -0300

    f ix

--- tmp/app.js
+++
@@ -1,9 +0,0 @@
-const mongojs = require('mongojs');
-const db = mongojs('mongodb://127.0.0.1/cursodb', ['alumnos','logs']);
-
-db.alumnos
-	.find({dni:3879047})
-	.limit(100)
-	.toArray((err, docs)=>{
-		console.log(err, docs);
-	});
